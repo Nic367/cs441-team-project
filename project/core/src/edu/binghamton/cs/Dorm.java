9b6b79de4353860b120e8110f637d7affb2f4ca3
ajahni1016 <alee884@binghamton.edu>
2021-11-17 09:54:24 -0500

    Added four screens for each minigame. Added a buttons to go to each screen

---
+++ project/core/src/edu/binghamton/cs/Dorm.java
@@ -0,0 +1,133 @@
+package edu.binghamton.cs;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Dorm {
+    String[] states= {"dorm_loop", "sleep_minigame", "food_minigame", "study_minigame", "sort_minigame"};
+    String gameState = states[0];
+
+    //Renderables
+    Stage stage;
+    SpriteBatch batch;
+    Texture img;
+
+
+    //Game Background
+    Texture bg;
+    TextureRegion region;
+
+    //Sleep Button
+    Texture sleepImg;
+    TextureRegion sleepRegion;
+    TextureRegionDrawable sleepDrawable;
+    ImageButton sleepButton;
+
+    //Study Button
+    Texture studyImg;
+    TextureRegion studyRegion;
+    TextureRegionDrawable studyDrawable;
+    ImageButton studyButton;
+
+    //Fitness Button
+    Texture fitnessImg;
+    TextureRegion fitnessRegion;
+    TextureRegionDrawable fitnessDrawable;
+    ImageButton fitnessButton;
+
+    //Hunger Button
+    Texture hungerImg;
+    TextureRegion hungerRegion;
+    TextureRegionDrawable hungerDrawable;
+    ImageButton hungerButton;
+
+    public void create(){
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
+        //Sleep Button
+        sleepImg = new Texture(Gdx.files.internal("data/sleepButton.png"));
+        sleepRegion = new TextureRegion(sleepImg);
+        sleepDrawable = new TextureRegionDrawable(sleepRegion);
+        sleepButton = new ImageButton(sleepDrawable);
+        sleepButton.setSize(400,400);
+        sleepButton.setPosition(Gdx.graphics.getWidth()-500,40);
+        sleepButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                gameState = states[1];
+            }
+        });
+
+        //Study Button
+        studyImg = new Texture(Gdx.files.internal("data/studyButton.png"));
+        studyRegion = new TextureRegion(studyImg);
+        studyDrawable = new TextureRegionDrawable(studyRegion);
+        studyButton = new ImageButton(studyDrawable);
+        studyButton.setSize(400,400);
+        studyButton.setPosition(0,40);
+        studyButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                gameState = states[3];
+            }
+        });
+
+        //Fitness Button
+        fitnessImg = new Texture(Gdx.files.internal("data/fitnessButton.png"));
+        fitnessRegion = new TextureRegion(fitnessImg);
+        fitnessDrawable = new TextureRegionDrawable(fitnessRegion);
+        fitnessButton = new ImageButton(fitnessDrawable);
+        fitnessButton.setSize(200,200);
+        fitnessButton.setPosition(Gdx.graphics.getWidth()-300,500);
+        fitnessButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                gameState = states[4];
+            }
+        });
+
+        //Hunger Button
+        hungerImg = new Texture(Gdx.files.internal("data/hungerButton.png"));
+        hungerRegion = new TextureRegion(hungerImg);
+        hungerDrawable = new TextureRegionDrawable(hungerRegion);
+        hungerButton = new ImageButton(hungerDrawable);
+        hungerButton.setSize(400,400);
+        hungerButton.setPosition(0,500);
+        hungerButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                gameState = states[2];
+            }
+        });
+
+        //Background
+        bg = new Texture(Gdx.files.internal("data/bg.jpeg"));
+        region = new TextureRegion(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+
+        //Staging
+        stage = new Stage(new ScreenViewport());
+        stage.addActor(sleepButton);
+        stage.addActor(hungerButton);
+        stage.addActor(fitnessButton);
+        stage.addActor(studyButton);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    public void render(){
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(region,0,0);
+        batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+}
--- project/core/src/edu/binghamton/cs/TeamProject.java
+++ project/core/src/edu/binghamton/cs/TeamProject.java
@@ -1,31 +1,71 @@
 package edu.binghamton.cs;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class TeamProject extends ApplicationAdapter {
-	SpriteBatch batch;
+	//Game states
+	final String DORM = "dorm_loop";
+	final String SLEEP = "sleep_minigame";
+	final String HUNGER = "food_minigame";
+	final String STUDY = "study_minigame";
+	final String FITNESS = "sort_minigame";
+	public String gameState = DORM;
+	Dorm dorm;
+
+	//Renderables
+	SpriteBatch dorm_batch;
 	Texture img;
-	
+
+	//Back Button
+	Stage stage;
+	Texture backImg;
+	TextureRegion region;
+	TextureRegionDrawable drawable;
+	ImageButton button;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		dorm = new Dorm();
+		dorm.create();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		if(gameState==DORM){
+			dorm.render();
+			gameState = dorm.gameState;
+		}
+		else if(gameState==SLEEP){
+			ScreenUtils.clear(0,0, 1, 1);
+		}
+		else if(gameState==HUNGER){
+			ScreenUtils.clear(0, 1, 0, 1);
+		}
+		else if(gameState==STUDY){
+			ScreenUtils.clear(1, 1, 0, 1);
+		}
+		else if(gameState==FITNESS){
+			ScreenUtils.clear(1, 0, 1, 1);
+		}
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		dorm_batch.dispose();
 		img.dispose();
 	}
 }
