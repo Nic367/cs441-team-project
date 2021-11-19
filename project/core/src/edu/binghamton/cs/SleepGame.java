7d10603993b0ba9cc5cd32f90ea9206827f7a513
mngopi <83566574+mngopi@users.noreply.github.com>
2021-11-19 10:59:50 -0500

    SleepGame.java class to handle sleep minigame

---
+++ project/core/src/edu/binghamton/cs/SleepGame.java
@@ -0,0 +1,55 @@
+package edu.binghamton.cs;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SleepGame implements Screen {
+    Sleep game;
+
+    public SleepGame(Sleep game){
+        this.game = game;
+        game.batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "This is the sleep minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
