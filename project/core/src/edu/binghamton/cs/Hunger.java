545b70587b9b97e1517f102b65d1acafb6c6d965
ajahni1016 <alee884@binghamton.edu>
2021-11-19 11:00:15 -0500

    added a timer to the top left that displayed the elapsed time since the game started

--- project/core/src/edu/binghamton/cs/Hunger.java
+++ project/core/src/edu/binghamton/cs/Hunger.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.awt.Shape;
@@ -27,6 +28,10 @@ public class Hunger {
     SpriteBatch batch;
     BitmapFont font;
 
+    // Timer
+    BitmapFont timer_text;
+    long timer, initial_time=TimeUtils.nanoTime();;
+
     // Player
     ShapeRenderer hitbox;
     String player_facing= "left";
@@ -34,13 +39,29 @@ public class Hunger {
     int player_h = 200;
     int player_w = 100;
 
+    // Food
+    ShapeRenderer food;
+    int food_x;
+    int food_y;
+
+
     // Game Background
     Texture bg;
     TextureRegion region;
 
     public void create(){
         //Player
-        hitbox  =  new ShapeRenderer();
+        hitbox = new ShapeRenderer();
+
+        //Food
+        food = new ShapeRenderer();
+        food_x = 500;
+        food_y = 1000;
+
+        //Timer
+        timer_text = new BitmapFont();
+        timer_text.getData().setScale(5f);
+        timer = 0;
 
         batch = new SpriteBatch();
         font = new BitmapFont();
@@ -52,17 +73,22 @@ public class Hunger {
         Gdx.input.setInputProcessor(stage);
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT);
 
+        /* ====== Text ====== */
         batch.begin();
-        font.draw(batch, "This is the food minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        //font.draw(batch, "This is the food minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        timer_text.draw(batch,"0:"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        timer = TimeUtils.nanoTime() - initial_time-1000000000;
         batch.end();
 
-        //  PLAYER  //
-        //hitbox
+
+        /* ====== PLAYER ====== */
+        /* hitbox */
         hitbox.begin(ShapeRenderer.ShapeType.Filled);
-        hitbox.setColor(77,31,177,1);
+        hitbox.setColor(71/255f,31/255f,177/255f,1);
         hitbox.rect(player_x+215, player_y+140, player_w, player_h);
         hitbox.end();
-        //movement
+
+        /* movement */ //(Arrow keys for now but will add touch eventually)
         if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
             player_facing = "right";
             if(player_x<Gdx.graphics.getWidth()-330){ //Keeps player from walking off screen
@@ -75,6 +101,18 @@ public class Hunger {
                 player_x -= 4;
             }
         }
+
+
+        /* ====== FALLING FOOD ====== */
+        /* hitbox */
+        food.begin(ShapeRenderer.ShapeType.Filled);
+        food.setColor(71/255f,31/255f,0/255f,1);
+        food.rect(food_x, food_y, 100, 100);
+        food.end();
+        /* movement */
+        food_y -= 2;
+
+        /* ====== FALLING FOOD ====== */
 
 
         stage.act(Gdx.graphics.getDeltaTime());
