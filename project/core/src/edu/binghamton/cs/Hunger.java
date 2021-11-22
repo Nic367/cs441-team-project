8ecc971288a26bacc49efc74f8a8a0279c753eac
ajahni1016 <alee884@binghamton.edu>
2021-11-22 10:25:33 -0500

    Moved logic for the food object into its own class to make duplicating easier.

--- project/core/src/edu/binghamton/cs/Hunger.java
+++ project/core/src/edu/binghamton/cs/Hunger.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -16,8 +17,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
-import java.awt.Shape;
+import java.util.ArrayList;
 
 public class Hunger {
     String[] states= {"dorm_loop", "sleep_minigame", "food_minigame", "study_minigame", "sport_minigame"};
@@ -33,16 +33,16 @@ public class Hunger {
     long timer, initial_time=TimeUtils.nanoTime();;
 
     // Player
-    ShapeRenderer hitbox;
+    ShapeRenderer hitboxC;
+    Rectangle hitbox;
     String player_facing= "left";
     int player_x, player_y = 0; // x is changed by arrows. y should never change.
     int player_h = 200;
     int player_w = 100;
 
     // Food
-    ShapeRenderer food;
-    int food_x;
-    int food_y;
+    ArrayList<ShapeRenderer> foodList;
+    Food food;
 
 
     // Game Background
@@ -51,12 +51,17 @@ public class Hunger {
 
     public void create(){
         //Player
-        hitbox = new ShapeRenderer();
+        hitboxC = new ShapeRenderer();
+        hitbox = new Rectangle();
 
         //Food
-        food = new ShapeRenderer();
-        food_x = 500;
-        food_y = 1000;
+        food = new Food();
+//        foodRect = new Rectangle();
+//        food = new ShapeRenderer();
+//        foodList = new ArrayList<ShapeRenderer>();
+//        foodList.add(food);
+//        food_x = 500;
+//        food_y = 1000;
 
         //Timer
         timer_text = new BitmapFont();
@@ -76,19 +81,26 @@ public class Hunger {
         /* ====== Text ====== */
         batch.begin();
         //font.draw(batch, "This is the food minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
-        timer_text.draw(batch,"0:"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
-        timer = TimeUtils.nanoTime() - initial_time-1000000000;
+        if(timer/1_000_000_000 <10){
+            timer_text.draw(batch,"0:0"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        }
+        else{
+            timer_text.draw(batch,"0:"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+        }
+        timer = TimeUtils.nanoTime() - initial_time-2000000000;
         batch.end();
 
 
         /* ====== PLAYER ====== */
         /* hitbox */
-        hitbox.begin(ShapeRenderer.ShapeType.Filled);
-        hitbox.setColor(71/255f,31/255f,177/255f,1);
-        hitbox.rect(player_x+215, player_y+140, player_w, player_h);
-        hitbox.end();
+        hitbox.set(player_x+215, player_y+140, player_w, player_h);
 
-        /* movement */ //(Arrow keys for now but will add touch eventually)
+        hitboxC.begin(ShapeRenderer.ShapeType.Filled);
+        hitboxC.setColor(71/255f,31/255f,177/255f,1);
+        hitboxC.rect(player_x+215, player_y+140, player_w, player_h);
+        hitboxC.end();
+
+        /* movement */
         if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
             player_facing = "right";
             if(player_x<Gdx.graphics.getWidth()-330){ //Keeps player from walking off screen
@@ -105,17 +117,35 @@ public class Hunger {
 
         /* ====== FALLING FOOD ====== */
         /* hitbox */
-        food.begin(ShapeRenderer.ShapeType.Filled);
-        food.setColor(71/255f,31/255f,0/255f,1);
-        food.rect(food_x, food_y, 100, 100);
-        food.end();
-        /* movement */
-        food_y -= 2;
+        if(food!=null){
+            food.visiblebox.begin(ShapeRenderer.ShapeType.Filled);
+            food.visiblebox.setColor(71/255f,31/255f,0/255f,1);
+            food.visiblebox.rect(food.x, food.y, 100, 100);
+            food.rect.set(food.x, food.y, 100, 100);
+            food.visiblebox.end();
+            /* movement */
+            food.y -= 2;
 
-        /* ====== FALLING FOOD ====== */
+            if(hitbox.overlaps(food.rect)){
+                food.visiblebox = null;
+                food.rect = null;
+                food = null;
+            }
+        }
+
 
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+    }
+
+    static class Food{
+        ShapeRenderer visiblebox = new ShapeRenderer();
+        Rectangle rect = new Rectangle();
+        int x = 500;
+        int y = 1000;
+
+        Food(){
+        }
     }
 }
