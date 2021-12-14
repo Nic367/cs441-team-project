2c807a0b8aefb52d616d85a6b705b97a2e926a50
ajahni1016 <alee884@binghamton.edu>
2021-12-14 17:04:11 -0500

    Added finishing visual touches to hunger minigame.

--- project/core/src/edu/binghamton/cs/Hunger.java
+++ project/core/src/edu/binghamton/cs/Hunger.java
@@ -1,5 +1,7 @@
 package edu.binghamton.cs;
 
+import static edu.binghamton.cs.TeamProject.DORM;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
@@ -30,7 +32,7 @@ public class Hunger {
     // Renderables
     Stage stage;
     SpriteBatch batch;
-    BitmapFont font;
+    BitmapFont font, endText, pressToContinue;
 
     // Timer
     BitmapFont timer_text;
@@ -38,11 +40,13 @@ public class Hunger {
 
     // Player
     ShapeRenderer hitboxC;
+    Texture playerImg;
+    Texture playerImgFlipped;
     Rectangle hitbox;
     String player_facing= "left";
     int player_x, player_y = 0; // x is changed by arrows. y should never change.
-    int player_h = 200;
-    int player_w = 100;
+    int player_h = 400;
+    int player_w = 260;
 
     // Food
     ArrayList<Food> foodList;
@@ -59,10 +63,18 @@ public class Hunger {
     Texture bg;
     TextureRegion region;
 
+    //Back Button
+    Texture backImg;
+    TextureRegion backRegion;
+    TextureRegionDrawable backDrawable;
+    ImageButton backButton;
+
     public void create(){
         //Player
         hitboxC = new ShapeRenderer();
         hitbox = new Rectangle();
+        playerImg = new Texture(Gdx.files.internal("data/hunger/player.png"));
+        playerImgFlipped = new Texture(Gdx.files.internal("data/hunger/playerFlipped.png"));
 
         //Food
         foodList = new ArrayList<>();
@@ -79,148 +91,193 @@ public class Hunger {
         timer = 0;
 
         //Background
-        bg = new Texture(Gdx.files.internal("data/dorm/dormBG.png"));
+        bg = new Texture(Gdx.files.internal("data/hunger/hungerBG.png"));
+
+        //Back Button
+        backImg = new Texture(Gdx.files.internal("data/hunger/hungerBG.png"));
+        backRegion = new TextureRegion(backImg);
+        backDrawable = new TextureRegionDrawable(backRegion);
+        backButton = new ImageButton(backDrawable);
+        backButton.getImage().setScale(20f);
+        backButton.setPosition(0,0);
+        backButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                TeamProject.gameState = DORM;
+                foodCaught=0;
+                hearts=3;
+                hunger=0;
+
+                foodList.clear();
+            }
+        });
+
+        //Text
+        endText = new BitmapFont();
+        endText.getData().setScale(5f);
+        pressToContinue = new BitmapFont();
+        pressToContinue.getData().setScale(7f);
 
         batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.getData().setScale(15f);
+
+        //Back Button
+        backImg = new Texture(Gdx.files.internal("data/hunger/hungerBG.png"));
+        backRegion = new TextureRegion(backImg);
+        backDrawable = new TextureRegionDrawable(backRegion);
+        backButton = new ImageButton(backDrawable);
+        backButton.getImage().setScale(20f);
+        backButton.setPosition(0,0);
+        backButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                if(hearts==0){
+                    TeamProject.gameState = DORM;
+                    foodCaught=0;
+                    hearts=3;
+                    hunger=0;
+                    foodList.clear();
+                }
+            }
+        });
         stage = new Stage(new ScreenViewport());
+        stage.addActor(backButton);
     }
 
     public void render(){
 
         Gdx.input.setInputProcessor(stage);
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(bg,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //Background
-
-        /* ====== Top Health Bar  ====== */
-        int x = 20;
-        for(int i=0;i<hearts;i++){
-            batch.draw(head,x,Gdx.graphics.getHeight()-220,200,200);
-            x+=220;
-        }
-
-        /* ====== Text ====== */
-
-        //font.draw(batch, "This is the food minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
-        if(timer/1_000_000_000 <10){
-            timer_text.draw(batch,"0:0"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
-        }
-        else{
-            timer_text.draw(batch,"0:"+timer/1_000_000_000,Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
-        }
-        timer = TimeUtils.nanoTime() - initial_time-2000000000;
-        batch.end();
-
-
-        /* ====== PLAYER ====== */
-        /* hitbox */
-        hitbox.set(player_x+215, player_y+140, player_w, player_h);
-
-        hitboxC.begin(ShapeRenderer.ShapeType.Filled);
-        hitboxC.setColor(71/255f,31/255f,177/255f,1);
-        hitboxC.rect(player_x+215, player_y+140, player_w, player_h);
-        hitboxC.end();
-
-        /* movement */
-        if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
-            player_facing = "right";
-            if(player_x<Gdx.graphics.getWidth()-330){ //Keeps player from walking off screen
-                player_x += 8;
-            }
-        }
-        else if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)){
-            player_facing = "left";
-            if(player_x>-200){ //Keeps player from walking off screen
-                player_x -= 8;
-            }
-        }
-
-
-        /* ====== FALLING FOOD ====== */
-        /* spawning */
-        int rand = ThreadLocalRandom.current().nextInt(1,3)%Gdx.graphics.getWidth();
-
-        if(foodTimer >= rand){
-            food = new Food(foodList);
-            foodTimer=0;
-        }
-        else{
-            foodTimer+=Gdx.graphics.getDeltaTime();
-        }
-
-        /* hitbox */
-        for(Food food : foodList){
-            if(food!=null){
-                if(food.goodOrBad==2 || food.goodOrBad==3){ //The food is one you want to catch
-                    food.rect.set(food.x, food.y, 200, 200);
-                    food.visiblebox.end();
-
-                    food.batch.begin(); //Drawing Book
-                    food.batch.draw(food.texture,food.x,food.y,200,200);
-                    food.batch.end();
-                }
-                else{ // The food is rotten/garbage and you don't want to catch it
-                    food.rect.set(food.x, food.y, 200, 200);
-                    food.batch.begin(); //Drawing Book
-                    food.batch.draw(food.texture,food.x,food.y,200,200);
-                    food.batch.end();
-
-                    //Hygeine down
-                    food.visiblebox.end();
-                }
-
-                /* movement */
-                food.y -= 15;
-            }
-
-            if(hitbox.overlaps(food.rect)){ //If the player catches the food
-                caughtFoodList.add(food);
-                foodCaught++;
-            }
-
-            if(food.y<= 20){  //Check if food hit the ground
-                fallenFoodList.add(food);
-            }
-        }
-
-        for(Food food:fallenFoodList){ //Un-rendering food that hit the ground
-            food.visiblebox = null;
-            food.rect = null;
-            foodList.remove(food);
-            //Remove health
-        }
-        for(Food food:caughtFoodList){ //Un-rendering food that the player caught
-            food.visiblebox = null;
-            food.rect = null;
-            foodList.remove(food);
-            if(food.goodOrBad==4 || food.goodOrBad==1){
-                hearts--;
-                if(Dorm.needs[2]>0){
-                    Dorm.needs[2]--; //hygiene down
-                    Dorm.updateStatusBars(2);
-                }
-            }
-            else{
-                if(Dorm.needs[4]<8){
-                    Dorm.needs[4]--;
-                    Dorm.updateStatusBars(4);
-                }
-            }
-            //Stack Sandwich
-        }
-
-        caughtFoodList.clear(); //Clearing the temp lists
-        fallenFoodList.clear();
-
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
 
         if(hearts==0){
-            TeamProject.gameState = states[0];
-            foodCaught=0;
-            hearts=3;
+            stage.draw();
+            batch.begin();
+            batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //Background
+            endText.setColor(0, 0, 0, 1);
+            endText.draw(batch,"Hunger increased by "+hunger+"\nHygiene decreased by "+hygiene,50,Gdx.graphics.getHeight()-50,Gdx.graphics.getWidth(), 5,true);
+            pressToContinue.setColor(0, 0, 0, 1);
+            pressToContinue.draw(batch,"Tap screen to continue...",40,150,Gdx.graphics.getWidth(), 5,true);
+            batch.end();
+        }
+        else {
+            batch.begin();
+            batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //Background
+
+            /* ====== Top Health Bar  ====== */
+            int x = 20;
+            for (int i = 0; i < hearts; i++) {
+                batch.draw(head, x, Gdx.graphics.getHeight() - 220, 200, 200);
+                x += 220;
+            }
+
+            /* ====== Text ====== */
+
+            //font.draw(batch, "This is the food minigame", 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth(),10,true);
+            if (timer / 1_000_000_000 < 10) {
+                timer_text.draw(batch, "0:0" + timer / 1_000_000_000, Gdx.graphics.getWidth() - 200, Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), 10, true);
+            } else {
+                timer_text.draw(batch, "0:" + timer / 1_000_000_000, Gdx.graphics.getWidth() - 200, Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), 10, true);
+            }
+            timer = TimeUtils.nanoTime() - initial_time - 2000000000;
+
+            /* ====== PLAYER ====== */
+            /* hitbox */
+            hitbox.set(player_x + 215, player_y, player_w, player_h);
+            if(player_facing=="right"){
+                batch.draw(playerImg, player_x + 215, player_y, player_w, player_h);
+            }
+            else{
+                batch.draw(playerImgFlipped, player_x + 215, player_y, player_w, player_h);
+            }
+
+            /* movement */
+            if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+                player_facing = "right";
+                if (player_x < Gdx.graphics.getWidth() - 330) { //Keeps player from walking off screen
+                    player_x += 8;
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+                player_facing = "left";
+                if (player_x > -200) { //Keeps player from walking off screen
+                    player_x -= 8;
+                }
+            }
+
+
+            /* ====== FALLING FOOD ====== */
+            /* spawning */
+            int rand = ThreadLocalRandom.current().nextInt(1, 3) % Gdx.graphics.getWidth();
+
+            if (foodTimer >= rand) {
+                food = new Food(foodList);
+                foodTimer = 0;
+            } else {
+                foodTimer += Gdx.graphics.getDeltaTime();
+            }
+
+            /* hitbox */
+            for (Food food : foodList) {
+                if (food != null) {
+                    if (food.goodOrBad == 2 || food.goodOrBad == 3) { //The food is one you want to catch
+                        food.rect.set(food.x, food.y, 200, 200);
+                        food.visiblebox.end();
+
+                        food.batch.begin(); //Drawing Book
+                        food.batch.draw(food.texture, food.x, food.y, 200, 200);
+                        food.batch.end();
+                    } else { // The food is rotten/garbage and you don't want to catch it
+                        food.rect.set(food.x, food.y, 200, 200);
+                        food.batch.begin(); //Drawing Book
+                        food.batch.draw(food.texture, food.x, food.y, 200, 200);
+                        food.batch.end();
+
+                        //Hygeine down
+                        food.visiblebox.end();
+                    }
+
+                    /* movement */
+                    food.y -= 15;
+                }
+
+                if (hitbox.overlaps(food.rect)) { //If the player catches the food
+                    caughtFoodList.add(food);
+                    foodCaught++;
+                }
+
+                if (food.y <= 20) {  //Check if food hit the ground
+                    fallenFoodList.add(food);
+                }
+            }
+
+            for (Food food : fallenFoodList) { //Un-rendering food that hit the ground
+                food.visiblebox = null;
+                food.rect = null;
+                foodList.remove(food);
+                //Remove health if good food was dropped
+                if (food.goodOrBad == 2 || food.goodOrBad == 3) {
+                    hearts--;
+                }
+            }
+            for (Food food : caughtFoodList) { //Un-rendering food that the player caught
+                food.visiblebox = null;
+                food.rect = null;
+                foodList.remove(food);
+                if (food.goodOrBad == 4 || food.goodOrBad == 1) {
+                    hearts--;
+                    if (Dorm.needs[2] > 0) {
+                        Dorm.needs[2]--; //hygiene down
+                        hygiene++;
+                        Dorm.updateStatusBars(2);
+                    }
+                } else {
+                    if (Dorm.needs[4] < 8) {
+                        Dorm.needs[4]++;
+                        hunger++;
+                        Dorm.updateStatusBars(4);
+                    }
+                }
+            }
+
+            caughtFoodList.clear(); //Clearing the temp lists
+            fallenFoodList.clear();
+            batch.end();
         }
     }
 
